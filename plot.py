17935963bcab57b2af154fd8106520161e75e627
Marlesss <334874@niuitmo.ru>
2023-03-06 18:40:45 +0300

    show_2arg_func added

--- plot.py
+++ plot.py
@@ -4,7 +4,7 @@ from mpl_toolkits import mplot3d
 import numpy as np
 from vector import Vector
 
-GRID_SIZE = 10
+GRID_SIZE = 100
 
 
 def wrap_func(func: Callable[[Vector], float]) -> Callable[[[float]], float]:
@@ -26,7 +26,7 @@ def unzip_dots(dots: [Vector]) -> [[float]]:
     return ans
 
 
-def show_2d_plot(func: Callable[[Vector], float], dots: [Vector]):
+def show_1arg_func(func: Callable[[Vector], float], dots: [Vector]):
     subplot = plt.subplot()
     min_x = min(map(lambda dot: dot[0], dots))
     max_x = max(map(lambda dot: dot[0], dots))
@@ -38,53 +38,47 @@ def show_2d_plot(func: Callable[[Vector], float], dots: [Vector]):
     plt.show()
 
 
-def func(x: Vector) -> float:
-    assert len(x) == 1
-    return x[0] ** 2
+def show_2arg_func(func: Callable[[Vector, Vector], float], dots: [Vector]):
+    x_line, y_line = unzip_dots(dots)
+    k = 2
+    grid = np.mgrid[min(x_line) / k:max(x_line) * k:complex(0, GRID_SIZE),
+           min(y_line) / k:max(y_line) * k:complex(0, GRID_SIZE)].reshape(2, -1).T
+    print(grid)
+    func_values = np.array(list(map(func, grid)))
+    minz, maxz = min(func_values), max(func_values)
+    medz = (minz + maxz) / 5
+
+    subplot = plt.subplot()
+    for i in range(GRID_SIZE ** 2):
+        z = func_values[i]
+        if z < medz:
+            c = (z - minz) / (medz - minz)
+            subplot.plot([grid.T[0][i]], [grid.T[1][i]], "ro", color=(0, 1 - c, c))
+        else:
+            c = (z - medz) / (maxz - medz)
+            subplot.plot([grid.T[0][i]], [grid.T[1][i]], "ro", color=(c, 0, 1 - c))
+    subplot.plot(x_line, y_line, "ro", color=(1, 1, 1))
+    subplot.plot(x_line, y_line, linewidth=1, color=(1, 1, 1))
+    plt.show()
+
+# def func(x: Vector) -> float:
+#     assert len(x) == 1
+#     return x[0] ** 2
 
 
 # dots = [Vector(-10, 1), Vector(-2, 2), Vector(10, 100)]
+
+
+# dots = [[20, 20], [5.318943338572119, 2.6002291420114005], [2.3215306705179453, 4.285782032031778],
+#         [2.0592856709478977, 3.974998164989072], [2.00574344289354, 4.005104460614513],
+#         [2.001059008973999, 3.9995534327991282], [2.0001025940580357, 4.000091172695428],
+#         [2.0000189168814164, 3.9999920236948685], [2.0000018319421162, 4.00000162885025],
+#         [2.0000003378495244, 3.999999857576605]]
 #
-# show_2d_plot(func, dots)
-
-dots = [[20, 20], [5.318943338572119, 2.6002291420114005], [2.3215306705179453, 4.285782032031778],
-        [2.0592856709478977, 3.974998164989072], [2.00574344289354, 4.005104460614513],
-        [2.001059008973999, 3.9995534327991282], [2.0001025940580357, 4.000091172695428],
-        [2.0000189168814164, 3.9999920236948685], [2.0000018319421162, 4.00000162885025],
-        [2.0000003378495244, 3.999999857576605]]
-
-
-def test_1_function(x: Vector) -> float:
-    assert len(x) == 2
-    return 2 * ((x[0] - 2) ** 2) + 4 * ((x[1] - 4) ** 2)
-
-
-fig = plt.figure()
-ax = plt.axes(projection='3d')
-
-# Data for a three-dimensional line
-# zline = np.linspace(0, 15, len(dots))
-# xline, yline = unzip_dots(dots)
-# ax.plot3D(xline, yline, zline, 'gray')
-
-# # Data for three-dimensional scattered points
-# zdata = 15 * np.random.random(100)
-# xdata = np.sin(zdata) + 0.1 * np.random.randn(100)
-# ydata = np.cos(zdata) + 0.1 * np.random.randn(100)
-# ax.scatter3D(xdata, ydata, zdata, cmap='Greens')
-
-# x_dots, y_dots = unzip_dots(dots)
-# xline = np.linspace(min(x_dots), max(x_dots), GRID_SIZE)
-# yline = np.linspace(min(y_dots), max(y_dots), GRID_SIZE)
-# zline = np.vectorize(test_1_function)(dots)
-# zline = np.vectorize(wrap_func(test_1_function))(*dots)
-# print(zline)
-
-# plt.show()
-
-gridd = np.mgrid[0:10:0.5, 0:10:0.5].reshape(2, -1).T
-zline = np.array(list(map(test_1_function, gridd)))
-for i in range(len(zline)):
-    print(gridd.T[0][i], gridd.T[1][i], zline[i])
-ax.scatter3D(gridd.T[0], gridd.T[1], zline)
-plt.show()
+#
+# def test_1_function(x: Vector) -> float:
+#     assert len(x) == 2
+#     return 2 * ((x[0] - 2) ** 2) + 4 * ((x[1] - 4) ** 2)
+#
+#
+# show_2arg_func(test_1_function, dots)
