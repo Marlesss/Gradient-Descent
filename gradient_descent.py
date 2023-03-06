b80adcf722a0b258ba8cedbcf05a6b2e5d4f6a4c
hedgehogushka <100299564+hedgehogushka@users.noreply.github.com>
2023-03-06 14:03:01 +0400

    I guess 2 point is done

--- gradient_descent.py
+++ gradient_descent.py
@@ -3,14 +3,38 @@ from vector import Vector
 
 ITER_LIMIT = 50
 EPS = 10 ** (-5)
+PHI = (1 + 5 ** 0.5)/2
 
+def golden_section_method(x: Vector, grad: Callable[[Vector], Vector], f: Callable[[Vector], float]) -> [Vector]:
+    left = 0
+    right = 1
+    med_left = left + (right - left)/(PHI + 1)
+    med_right = right - (right - left)/(PHI + 1)
+    f_left = f(x - med_left * grad(x))
+    f_right = f(x - med_right * grad(x))
 
-def gradient_descent(x0: Vector, alph: float, grad: Callable[[Vector], Vector]) -> (bool, [Vector]):
+    while (abs(left - right) > EPS) :
+        if (f_left < f_right):
+            right = med_right
+            med_right = med_left
+            f_right = f_left
+            med_left = left + (right - left)/(PHI + 1)
+            f_left = f(x - med_left * grad(x))
+        else:
+            left = med_left
+            med_left = med_right
+            f_left = f_right
+            med_right = right - (right - left)/(PHI + 1)
+            f_right = f(x - med_right * grad(x))
+
+    return x - left * grad(x)
+
+def gradient_descent(x0: Vector, grad: Callable[[Vector], Vector], f: Callable[[Vector], float]) -> (bool, [Vector]):
     eps_dot = Vector([EPS for _ in range(len(x0))])
     dots = [x0]
     prev_x = x0
     for _ in range(ITER_LIMIT):
-        new_x = prev_x - alph * grad(prev_x)
+        new_x = golden_section_method(prev_x, grad, f)
         dots.append(new_x)
 
         # изменить на сравнение длины вектора
--- main.py
+++ main.py
@@ -2,13 +2,17 @@ from gradient_descent import gradient_descent
 from vector import Vector
 
 
-def test_1(x: Vector) -> Vector:
+def test_1_function(x: Vector) -> float:
+    assert len(x) == 2
+    return 2 * ((x[0] - 2) ** 2) + 4 * ((x[1] - 4) ** 2)
+
+def test_1_grad(x: Vector) -> Vector:
     assert len(x) == 2
     return Vector([(x[0] - 2) * 6, (x[1] - 4) * 8])
 
 
 def main():
-    ans, dots = gradient_descent(Vector([0, 0]), 0.1, test_1)
+    ans, dots = gradient_descent(Vector([20, 20]), test_1_grad, test_1_function)
     if ans:
         print(f"Сошлось за {len(dots)} шагов")
         print(dots)
