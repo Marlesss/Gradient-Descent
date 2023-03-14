fc8975b07a90b56b365b92df33346e8b3090ea0f
Daniil Zanin <daniil.zanin@mail.ru>
2023-03-14 18:33:43 +0400

    precalculation of gradient(x) and f(x) in methods

--- gradient_descent.py
+++ gradient_descent.py
@@ -8,15 +8,15 @@ EPS = 10 ** -3
 PHI = (1 + 5 ** 0.5) / 2
 
 
-def first_wolfe_condition(x: np.ndarray, antigradient: np.ndarray, alpha: float,
+def first_wolfe_condition(x: np.ndarray, antigradient_x: np.ndarray, f_x: float, alpha: float,
                           c1: float, grad: Callable[[np.ndarray], np.ndarray],
                           f: Callable[[np.ndarray], float]) -> bool:
-    return f(x + alpha * antigradient) <= f(x) + c1 * alpha * np.dot(grad(x), antigradient)
+    return f(x + alpha * antigradient_x) <= f_x + c1 * alpha * np.dot(grad(x), antigradient_x)
 
 
-def second_wolfe_condition(x: np.ndarray, antigradient: np.ndarray, alpha: float,
+def second_wolfe_condition(x: np.ndarray, antigradient_x: np.ndarray, alpha: float,
                            c2: float, grad: Callable[[np.ndarray], np.ndarray]) -> bool:
-    return np.dot(grad(x + alpha * antigradient), antigradient) >= c2 * np.dot(grad(x), antigradient)
+    return np.dot(grad(x + alpha * antigradient_x), antigradient_x) >= c2 * np.dot(grad(x), antigradient_x)
 
 
 def golden_section_method_with_wolfe_conditions(x: np.ndarray, grad: Callable[[np.ndarray], np.ndarray],
@@ -24,11 +24,12 @@ def golden_section_method_with_wolfe_conditions(x: np.ndarray, grad: Callable[[n
                                                 c1: float, c2: float) -> float:
     left = 0
     right = 1
-    gradient = grad(x)
+    f_x = f(x)
+    gradient_x = grad(x)
     med_left = left + (right - left) / (PHI + 1)
     med_right = right - (right - left) / (PHI + 1)
-    f_left = f(x - med_left * gradient)
-    f_right = f(x - med_right * gradient)
+    f_left = f(x - med_left * gradient_x)
+    f_right = f(x - med_right * gradient_x)
 
     while abs(left - right) > EPS:
         if f_left < f_right:
@@ -36,16 +37,16 @@ def golden_section_method_with_wolfe_conditions(x: np.ndarray, grad: Callable[[n
             med_right = med_left
             f_right = f_left
             med_left = left + (right - left) / (PHI + 1)
-            f_left = f(x - med_left * gradient)
+            f_left = f(x - med_left * gradient_x)
         else:
             left = med_left
             med_left = med_right
             f_left = f_right
             med_right = right - (right - left) / (PHI + 1)
-            f_right = f(x - med_right * gradient)
+            f_right = f(x - med_right * gradient_x)
         checkpoint = (left + right) / 2
-        if (first_wolfe_condition(x, -gradient, checkpoint, c1, grad, f)
-                and second_wolfe_condition(x, -gradient, checkpoint, c2, grad)):
+        if (first_wolfe_condition(x, -gradient_x, f_x, checkpoint, c1, grad, f)
+                and second_wolfe_condition(x, -gradient_x, checkpoint, c2, grad)):
             return checkpoint
 
     return left
