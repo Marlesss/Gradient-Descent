77855ce289080af7df4f0dbf60b2d19e58b05abe
Daniil Zanin <daniil.zanin@mail.ru>
2023-03-19 16:41:50 +0400

    removed extra gradient calculation

--- gradient_descent.py
+++ gradient_descent.py
@@ -20,44 +20,12 @@ def second_wolfe_condition(x: np.ndarray, gradient_x: np.ndarray, direction: np.
 
 
 def golden_section_method_with_wolfe_conditions(x: np.ndarray, grad: Callable[[np.ndarray], np.ndarray],
-                                                f: Callable[[np.ndarray], float],
+                                                f: Callable[[np.ndarray], float], grad_value: np.array,
                                                 c1: float, c2: float) -> float:
     left = 0
     right = 1
     f_x = f(x)
-    gradient_x = grad(x)
-    direction = -grad(x)
-    med_left = left + (right - left) / (PHI + 1)
-    med_right = right - (right - left) / (PHI + 1)
-    f_left = f(x - med_left * gradient_x)
-    f_right = f(x - med_right * gradient_x)
-
-    while abs(left - right) > EPS:
-        if f_left < f_right:
-            right = med_right
-            med_right = med_left
-            f_right = f_left
-            med_left = left + (right - left) / (PHI + 1)
-            f_left = f(x - med_left * gradient_x)
-        else:
-            left = med_left
-            med_left = med_right
-            f_left = f_right
-            med_right = right - (right - left) / (PHI + 1)
-            f_right = f(x - med_right * gradient_x)
-        checkpoint = (left + right) / 2
-        if (first_wolfe_condition(x, gradient_x, direction, f_x, checkpoint, c1, f)
-                and second_wolfe_condition(x, gradient_x, direction, checkpoint, c2, grad)):
-            return checkpoint
-
-    return left
-
-
-def golden_section_method(x: np.ndarray, grad: Callable[[np.ndarray], np.ndarray],
-                          f: Callable[[np.ndarray], float]) -> float:
-    left = 0
-    right = 1
-    direction = -grad(x)
+    direction = -grad_value
     med_left = left + (right - left) / (PHI + 1)
     med_right = right - (right - left) / (PHI + 1)
     f_left = f(x + med_left * direction)
@@ -76,19 +44,49 @@ def golden_section_method(x: np.ndarray, grad: Callable[[np.ndarray], np.ndarray
             f_left = f_right
             med_right = right - (right - left) / (PHI + 1)
             f_right = f(x + med_right * direction)
-    return left
+        checkpoint = (left + right) / 2
+        if (first_wolfe_condition(x, grad_value, direction, f_x, checkpoint, c1, f)
+                and second_wolfe_condition(x, grad_value, direction, checkpoint, c2, grad)):
+            return checkpoint
+
+    return (right + left)/2
+
+
+def golden_section_method(x: np.ndarray, f: Callable[[np.ndarray], float], grad_value: np.ndarray) -> float:
+    left = 0
+    right = 1
+    direction = -grad_value
+    med_left = left + (right - left) / (PHI + 1)
+    med_right = right - (right - left) / (PHI + 1)
+    f_left = f(x + med_left * direction)
+    f_right = f(x + med_right * direction)
+
+    while abs(left - right) > EPS:
+        if f_left < f_right:
+            right = med_right
+            med_right = med_left
+            f_right = f_left
+            med_left = left + (right - left) / (PHI + 1)
+            f_left = f(x + med_left * direction)
+        else:
+            left = med_left
+            med_left = med_right
+            f_left = f_right
+            med_right = right - (right - left) / (PHI + 1)
+            f_right = f(x + med_right * direction)
+    return (right + left)/2
 
 
 def gradient_descent_linear_with_wolfe_condition(x0: np.ndarray, grad: Callable[[np.ndarray], np.ndarray],
-                                                 f: Callable[[np.ndarray], float], c1: float, c2: float) -> (
-bool, np.ndarray):
+                                                 f: Callable[[np.ndarray], float], c1: float, c2: float) \
+        -> (bool, np.ndarray):
     dots = np.array([x0])
     prev_x = x0
     for _ in range(ITER_LIMIT):
         grad_value = grad(prev_x)
         if sqrt((grad_value ** 2).sum()) < EPS:
             return True, dots
-        new_x = prev_x - golden_section_method_with_wolfe_conditions(prev_x, grad, f, c1, c2) * grad_value
+        new_x = prev_x - golden_section_method_with_wolfe_conditions(prev_x, grad, f, grad_value, c1, c2) * grad_value
         dots = np.append(dots, [new_x], 0)
         prev_x = new_x
     return False, dots
@@ -102,7 +100,7 @@ def gradient_descent_linear(x0: np.ndarray, grad: Callable[[np.ndarray], np.ndar
         grad_value = grad(prev_x)
         if sqrt((grad_value ** 2).sum()) < CONVERGENCE_EPS:
             return True, dots
-        new_x = prev_x - golden_section_method(prev_x, grad, f) * grad_value
+        new_x = prev_x - golden_section_method(prev_x, f, grad_value) * grad_value
         dots = np.append(dots, [new_x], 0)
         prev_x = new_x
     return False, dots
